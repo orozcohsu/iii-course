cde73b2c76af6130e020a3b7573907e57b6b413a
orozcohsu <orozcohsu@hotmail.com>
2019-09-14 09:08:01 +0800

    init

---
+++ profile.java
@@ -0,0 +1,366 @@
+// ORM class for table 'profile'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Fri Sep 13 23:55:29 UTC 2019
+// For connector: org.apache.sqoop.manager.GenericJdbcManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import com.cloudera.sqoop.lib.JdbcWritableBridge;
+import com.cloudera.sqoop.lib.DelimiterSet;
+import com.cloudera.sqoop.lib.FieldFormatter;
+import com.cloudera.sqoop.lib.RecordParser;
+import com.cloudera.sqoop.lib.BooleanParser;
+import com.cloudera.sqoop.lib.BlobRef;
+import com.cloudera.sqoop.lib.ClobRef;
+import com.cloudera.sqoop.lib.LargeObjectLoader;
+import com.cloudera.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class profile extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  protected ResultSet __cur_result_set;
+  private Integer id;
+  public Integer get_id() {
+    return id;
+  }
+  public void set_id(Integer id) {
+    this.id = id;
+  }
+  public profile with_id(Integer id) {
+    this.id = id;
+    return this;
+  }
+  private String uuid;
+  public String get_uuid() {
+    return uuid;
+  }
+  public void set_uuid(String uuid) {
+    this.uuid = uuid;
+  }
+  public profile with_uuid(String uuid) {
+    this.uuid = uuid;
+    return this;
+  }
+  private String name;
+  public String get_name() {
+    return name;
+  }
+  public void set_name(String name) {
+    this.name = name;
+  }
+  public profile with_name(String name) {
+    this.name = name;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof profile)) {
+      return false;
+    }
+    profile that = (profile) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.uuid == null ? that.uuid == null : this.uuid.equals(that.uuid));
+    equal = equal && (this.name == null ? that.name == null : this.name.equals(that.name));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof profile)) {
+      return false;
+    }
+    profile that = (profile) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.uuid == null ? that.uuid == null : this.uuid.equals(that.uuid));
+    equal = equal && (this.name == null ? that.name == null : this.name.equals(that.name));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.uuid = JdbcWritableBridge.readString(2, __dbResults);
+    this.name = JdbcWritableBridge.readString(3, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.uuid = JdbcWritableBridge.readString(2, __dbResults);
+    this.name = JdbcWritableBridge.readString(3, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeString(uuid, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(name, 3 + __off, 12, __dbStmt);
+    return 3;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeString(uuid, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(name, 3 + __off, 12, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.id = null;
+    } else {
+    this.id = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.uuid = null;
+    } else {
+    this.uuid = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.name = null;
+    } else {
+    this.name = Text.readString(__dataIn);
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.id);
+    }
+    if (null == this.uuid) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, uuid);
+    }
+    if (null == this.name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, name);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.id);
+    }
+    if (null == this.uuid) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, uuid);
+    }
+    if (null == this.name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, name);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 1, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(uuid==null?"null":uuid, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(name==null?"null":name, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(uuid==null?"null":uuid, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(name==null?"null":name, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 1, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    __cur_str = __it.next();
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Integer.valueOf(__cur_str);
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.uuid = null; } else {
+      this.uuid = __cur_str;
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.name = null; } else {
+      this.name = __cur_str;
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    __cur_str = __it.next();
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Integer.valueOf(__cur_str);
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.uuid = null; } else {
+      this.uuid = __cur_str;
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.name = null; } else {
+      this.name = __cur_str;
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    profile o = (profile) super.clone();
+    return o;
+  }
+
+  public void clone0(profile o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new TreeMap<String, Object>();
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("uuid", this.uuid);
+    __sqoop$field_map.put("name", this.name);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("uuid", this.uuid);
+    __sqoop$field_map.put("name", this.name);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if ("id".equals(__fieldName)) {
+      this.id = (Integer) __fieldVal;
+    }
+    else    if ("uuid".equals(__fieldName)) {
+      this.uuid = (String) __fieldVal;
+    }
+    else    if ("name".equals(__fieldName)) {
+      this.name = (String) __fieldVal;
+    }
+    else {
+      throw new RuntimeException("No such field: " + __fieldName);
+    }
+  }
+  public boolean setField0(String __fieldName, Object __fieldVal) {
+    if ("id".equals(__fieldName)) {
+      this.id = (Integer) __fieldVal;
+      return true;
+    }
+    else    if ("uuid".equals(__fieldName)) {
+      this.uuid = (String) __fieldVal;
+      return true;
+    }
+    else    if ("name".equals(__fieldName)) {
+      this.name = (String) __fieldVal;
+      return true;
+    }
+    else {
+      return false;    }
+  }
+}
