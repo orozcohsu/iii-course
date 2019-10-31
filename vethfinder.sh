389f33af34e3461624025761465efbd88653c53b
Orozco <orozcohsu@hotmail.com>
2019-10-31 13:51:37 +0800

    docker interface vethfinder

---
+++ vethfinder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for container in $(docker ps --format '{{.Names}}'); do
+    iflink=`docker exec -it $container bash -c 'cat /sys/class/net/eth*/iflink'`
+    for net in $iflink;do
+        net=`echo $net|tr -d '\r'`
+        veth=`grep -l $net /sys/class/net/veth*/ifindex`
+        veth=`echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;'`
+        echo $container:$veth
+    done
+done
